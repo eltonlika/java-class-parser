ac810957c70a0108a248b08d5fc6977e3263a12e
Elton Lika <eltonlika1@gmail.com>
2018-02-14 23:24:08 +0100

    implemented directory & jar file walking functions

--- bin/jjs-parser.js
+++ bin/jjs-parser.js
@@ -1,34 +1,42 @@
 #!/usr/bin/env node
 
 var parser = require('../');
-
 var args = process.argv.slice(2);
+var argv = require('minimist')(args);
+console.log(argv);
+var files = argv._;
+var printer = (argv['j'] || argv['json']) ? jsonPrinter : simplePrinter;
 
-parser(args, function(err, rs) {
-  if (err) 
-    return console.error(err.message);
+parser(files, printer);
 
-  for(var clzName in rs) {
-    var clz = rs[clzName];
+function simplePrinter(err, rs) {
+    if (err)
+        return console.error(err.message);
 
-    console.log('%s%s%s :', clzName, clz.extends.length ? (' extends ' + clz.extends.join(',')) :'', (clz.implements.length) ? (' implements ' + clz.implements.join(',')) : '');
-    clz.constructors.forEach(function(cons) {
-      console.log('\tconstructor: %s %s(%s)', cons.scope, cons.name, cons.args.join(','));
-    });
+    for (var clzName in rs) {
+        var clz = rs[clzName];
 
-    console.log('\tfields:');
-    clz.fields.forEach(function(f) {
-      console.log('\t\t%s %s %s', f.scope, f.type, f.name);
-    });
+        console.log('%s%s%s :', clzName, clz.extends.length ? (' extends ' + clz.extends.join(',')) : '', (clz.implements.length) ? (' implements ' + clz.implements.join(',')) : '');
+        clz.constructors.forEach(function(cons) {
+            console.log('\tconstructor: %s %s(%s)', cons.scope, cons.name, cons.args.join(','));
+        });
 
-    console.log('\tmethods:');
-    clz.methods.forEach(function(method) {
-      console.log('\t\t%s %s %s(%s)', method.scope, method.ret, method.name, method.args.join(','));
-    });
-    console.log();
-  }
-});
+        console.log('\tfields:');
+        clz.fields.forEach(function(f) {
+            console.log('\t\t%s %s %s', f.scope, f.type, f.name);
+        });
 
+        console.log('\tmethods:');
+        clz.methods.forEach(function(method) {
+            console.log('\t\t%s %s %s(%s)', method.scope, method.ret, method.name, method.args.join(','));
+        });
+        console.log();
+    }
+}
 
+function jsonPrinter(err, rs) {
+    if (err)
+        return console.error(err.message);
 
-
+    console.log(JSON.stringify(rs, null, 2));
+}
